8dfff1effe828c9b7cfcdec22d43cc0c6a4c4d92
chengyoufu <chengyoufu@gmail.com>
2015-10-05 21:32:07 +0800

    fff

--- study/method/func_paratest.py
+++ study/method/func_paratest.py
@@ -12,16 +12,32 @@
 @time: 2015/10/3 21:03
 """
 
+def power(x):
+    return  x*x
 
-def story(**kwds):
+def power(x,n=2):
+    s = 1
+    while n >0:
+        n-=1
+        s=s*x
+    return s
 
 
+def add_end(L=None):#默认参数必须指向不变对象
+    if L is None:
+       L = []
 
+    L.append('END')
+    return L
 
-class Main():
-    def __init__(self)
-        pass
+L1=add_end([1,2,3])
+print(L1)
 
+L2=add_end(['a','b','c'])
+print(L2)
 
-if __name__ == '__main__':
-    pass
+L3=add_end()
+print(L3)
+
+L4=add_end()
+print(L4)
