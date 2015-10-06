03b55c7c8200dd1ee901bdaa74cf6611e24eb5b8
chengyoufu <chengyoufu@gmail.com>
2015-10-06 22:37:06 +0800

    commit

---
+++ study/functional/filter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+@version: ??
+@author: chengyoufu
+@license: Apache Licence 
+@contact: chengyoufu@gmail.com
+@site: http://www.xxxxx.com
+@software: PyCharm
+@file: filter.py
+@time: 2015/10/6 22:24
+"""
+
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+
+
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break
