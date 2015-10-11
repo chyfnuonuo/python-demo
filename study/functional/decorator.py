7c76a37da07e5d6f319bb8587cfd148ec696134b
chengyoufu <chengyoufu@gmail.com>
2015-10-11 21:06:19 +0800

    commit

---
+++ study/functional/decorator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+@version: ??
+@author: chengyoufu
+@license: Apache Licence 
+@contact: chengyoufu@gmail.com
+@site: http://www.xxxxx.com
+@software: PyCharm
+@file: decorator.py
+@time: 2015/10/10 20:49
+"""
+import time
+
+
+def deco(func):
+    def wrapper(a, b):
+        startTime = time.time()
+        ret = func(a, b)
+        endTime = time.time()
+        msecs = (endTime - startTime) * 1000
+        print(">>elapesd time:%f ms" % msecs)
+
+    return wrapper
+
+
+@deco
+def myfunc(a, b):
+    print('start myfunc')
+    time.sleep(0.6)
+    print('result is %d' % (a + b))
+    print('end myfunc')
+
+
+myfunc(3, 8)
---
+++ study/functional/returnfunc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+@version: ??
+@author: chengyoufu
+@license: Apache Licence 
+@contact: chengyoufu@gmail.com
+@site: http://www.xxxxx.com
+@software: PyCharm
+@file: returnfunc.py
+@time: 2015/10/10 20:44
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self)
+        pass
+
+
+if __name__ == '__main__':
+    pass
